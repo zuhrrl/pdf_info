f5321c489cfe00246d28acf7cd3f0173fd592c56
Zuhrul Anam <zuhrrlanam@gmail.com>
2024-05-19 15:49:55 +0700

    feat: remove doc after upload

--- main.py
+++ main.py
@@ -2,9 +2,20 @@
 import pypdfium2 as pdfium
 from fastapi import FastAPI, File, UploadFile
 import shutil
+import os
+import uuid
+
 
 PAGEINDEX = 0  # the first page
 FILEPATH = ""
+
+
+def getRandomString(string_length=10):
+    """Returns a random string of length string_length."""
+    random = str(uuid.uuid4()) # Convert UUID format to a Python string.
+    random = random.upper() # Make all characters uppercase.
+    random = random.replace("-","") # Remove the UUID '-'.
+    return random[0:string_length]
 
 def getOrientation(width, height):
     if width > height:
@@ -19,12 +30,15 @@ def root():
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-    path = f"files/document.pdf"
+    fileName = getRandomString(20)
+    path = f"files/{fileName}.pdf"
     with open(path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-
-    pdf = pdfium.PdfDocument('files/document.pdf')
+    filePath = 'files/' + fileName + '.pdf'
+    print(filePath)
+    pdf = pdfium.PdfDocument(filePath)
     width, height = pdf.get_page_size(PAGEINDEX)
+    os.remove(filePath) 
     
     return {"status": True, "message" : "success get pdf info", "original_file_name": file.filename, 'width': round(width), 'height': round(height), 'orientation': getOrientation(width, height)}
 
